fdf973aa9dcf88e3696c0f263508408368c5b53c
IQisMySenpai <jannick.schroer@gmail.com>
2021-05-11 23:27:21 +0200

    First Botcode

---
+++ bot.py
@@ -0,0 +1,13 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('ODQxNzc3Njg2OTU4MzA5NDQ2.YJrskA.XBbTbMxdZVSPzONfUD1_lVfku_o')
+
+client = discord.Client()
+
+
+
+client.run(TOKEN)
