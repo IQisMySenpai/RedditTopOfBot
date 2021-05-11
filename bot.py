cbf1be1f181a764a92f9d2b29bffa0dcb1c8b957
IQisMySenpai <jannick.schroer@gmail.com>
2021-05-11 23:56:52 +0200

    First Command

--- bot.py
+++ bot.py
@@ -8,6 +8,13 @@ TOKEN = os.getenv('ODQxNzc3Njg2OTU4MzA5NDQ2.YJrskA.XBbTbMxdZVSPzONfUD1_lVfku_o')
 
 client = discord.Client()
 
-# Add Code Here
+bot = commands.Bot(command_prefix='$')
+
+@bot.command()
+async def postrepeat(ctx, subreddit="r/ProgrammerHumor", freqhours: int = 24, topof="week"):
+    
+
+    response = "Test"
+    await ctx.send(response)
 
 client.run(TOKEN)
