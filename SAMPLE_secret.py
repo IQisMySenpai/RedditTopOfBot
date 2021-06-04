f7ebcda9c0bc5d1420d382e1b6e70e3a74cf9e74
AliSot2000 <44428989+AliSot2000@users.noreply.github.com>
2021-06-04 22:13:54 +0200

    Added a example for the secret.py file.

---
+++ SAMPLE_secret.py
@@ -0,0 +1,12 @@
+# **********************************************************************************************************************
+# DO NOT COMMIT THIS FILE TO GITHUB WITH ACTUAL KEYS!!!
+# **********************************************************************************************************************
+
+# File contains all the secret informations which MAY NOT BE SENT TO A PUBLIC REPO!!!
+TOKEN = "DISCORD TOKEN"
+db_username = "USERNAME TO ACCESS DB"
+db_password = r"PASSWORD TO ACCESS DB"
+db_name = "NAME OF DATABSE"
+col_queue = "COLLECTION NAME OF THE QUEUES"
+col_servers = "COLLECTION NAME OF THE SERVERS"
+
